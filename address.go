baace834d6c96e6ce8fae721d0690a44999ffd51
Joseph Phillips <joe.phillips@canonical.com>
2019-10-28 17:05:16 +0100

    Adds version 2 of Address that supports the presence of space IDs.

--- address.go
+++ address.go
@@ -14,24 +14,27 @@ type Address interface {
 	Type() string
 	Scope() string
 	Origin() string
+	SpaceID() string
 }
 
 // AddressArgs is an argument struct used to create a new internal address
 // type that supports the Address interface.
 type AddressArgs struct {
-	Value  string
-	Type   string
-	Scope  string
-	Origin string
+	Value   string
+	Type    string
+	Scope   string
+	Origin  string
+	SpaceID string
 }
 
 func newAddress(args AddressArgs) *address {
 	return &address{
-		Version: 1,
-		Value_:  args.Value,
-		Type_:   args.Type,
-		Scope_:  args.Scope,
-		Origin_: args.Origin,
+		Version:  2,
+		Value_:   args.Value,
+		Type_:    args.Type,
+		Scope_:   args.Scope,
+		Origin_:  args.Origin,
+		SpaceID_: args.SpaceID,
 	}
 }
 
@@ -39,10 +42,11 @@ func newAddress(args AddressArgs) *address {
 type address struct {
 	Version int `yaml:"version"`
 
-	Value_  string `yaml:"value"`
-	Type_   string `yaml:"type"`
-	Scope_  string `yaml:"scope,omitempty"`
-	Origin_ string `yaml:"origin,omitempty"`
+	Value_   string `yaml:"value"`
+	Type_    string `yaml:"type"`
+	Scope_   string `yaml:"scope,omitempty"`
+	Origin_  string `yaml:"origin,omitempty"`
+	SpaceID_ string `yaml:"spaceid,omitempty"`
 }
 
 // Value implements Address.
@@ -63,6 +67,11 @@ func (a *address) Scope() string {
 // Origin implements Address.
 func (a *address) Origin() string {
 	return a.Origin_
+}
+
+// SpaceID implements Address.
+func (a *address) SpaceID() string {
+	return a.SpaceID_
 }
 
 func importAddresses(sourceList []interface{}) ([]*address, error) {
@@ -101,20 +110,11 @@ type addressDeserializationFunc func(map[string]interface{}) (*address, error)
 
 var addressDeserializationFuncs = map[int]addressDeserializationFunc{
 	1: importAddressV1,
+	2: importAddressV2,
 }
 
 func importAddressV1(source map[string]interface{}) (*address, error) {
-	fields := schema.Fields{
-		"value":  schema.String(),
-		"type":   schema.String(),
-		"scope":  schema.String(),
-		"origin": schema.String(),
-	}
-	// Some values don't have to be there.
-	defaults := schema.Defaults{
-		"scope":  "",
-		"origin": "",
-	}
+	fields, defaults := addressV1Fields()
 	checker := schema.FieldMap(fields, defaults)
 
 	coerced, err := checker.Coerce(source, nil)
@@ -132,4 +132,53 @@ func importAddressV1(source map[string]interface{}) (*address, error) {
 		Scope_:  valid["scope"].(string),
 		Origin_: valid["origin"].(string),
 	}, nil
+}
+
+func importAddressV2(source map[string]interface{}) (*address, error) {
+	fields, defaults := addressV1Fields()
+	fields["spaceid"] = schema.String()
+
+	// We must allow for an empty space ID because:
+	// - newAddress always returns a V2 address.
+	// - newAddress is called by methods in Machine that do not negotiate a
+	//   version.
+	// If an old version of Juju not supporting address spaces upgrades to this
+	// version of the library, we need to allow export and import of V2
+	// addresses that tolerate a missing space ID.
+	// Ensuring correct defaults for this field must be ensured in the Juju
+	// migration code itself.
+	defaults["spaceid"] = ""
+	checker := schema.FieldMap(fields, defaults)
+
+	coerced, err := checker.Coerce(source, nil)
+	if err != nil {
+		return nil, errors.Annotatef(err, "address v1 schema check failed")
+	}
+	valid := coerced.(map[string]interface{})
+	// From here we know that the map returned from the schema coercion
+	// contains fields of the right type.
+
+	return &address{
+		Version:  2,
+		Value_:   valid["value"].(string),
+		Type_:    valid["type"].(string),
+		Scope_:   valid["scope"].(string),
+		Origin_:  valid["origin"].(string),
+		SpaceID_: valid["spaceid"].(string),
+	}, nil
+}
+
+func addressV1Fields() (schema.Fields, schema.Defaults) {
+	fields := schema.Fields{
+		"value":  schema.String(),
+		"type":   schema.String(),
+		"scope":  schema.String(),
+		"origin": schema.String(),
+	}
+	// Some values don't have to be there.
+	defaults := schema.Defaults{
+		"scope":  "",
+		"origin": "",
+	}
+	return fields, defaults
 }
