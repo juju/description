e27246c9d1c19d12958e8ec55eb4da29d8e21f74
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2020-06-24 11:46:19 +0100

    Add VirtualPortType to Link Layer Device schema

--- linklayerdevice_test.go
+++ linklayerdevice_test.go
@@ -29,15 +29,16 @@ func (s *LinkLayerDeviceSerializationSuite) SetUpTest(c *gc.C) {
 
 func (s *LinkLayerDeviceSerializationSuite) TestNewLinkLayerDevice(c *gc.C) {
 	args := LinkLayerDeviceArgs{
-		ProviderID:  "magic",
-		MachineID:   "bar",
-		Name:        "foo",
-		MTU:         54,
-		Type:        "loopback",
-		MACAddress:  "DEADBEEF",
-		IsAutoStart: true,
-		IsUp:        true,
-		ParentName:  "bam",
+		ProviderID:      "magic",
+		MachineID:       "bar",
+		Name:            "foo",
+		MTU:             54,
+		Type:            "loopback",
+		MACAddress:      "DEADBEEF",
+		IsAutoStart:     true,
+		IsUp:            true,
+		ParentName:      "bam",
+		VirtualPortType: "ovs",
 	}
 	device := newLinkLayerDevice(args)
 	c.Assert(device.ProviderID(), gc.Equals, args.ProviderID)
@@ -49,9 +50,10 @@ func (s *LinkLayerDeviceSerializationSuite) TestNewLinkLayerDevice(c *gc.C) {
 	c.Assert(device.IsAutoStart(), gc.Equals, args.IsAutoStart)
 	c.Assert(device.IsUp(), gc.Equals, args.IsUp)
 	c.Assert(device.ParentName(), gc.Equals, args.ParentName)
+	c.Assert(device.VirtualPortType(), gc.Equals, args.VirtualPortType)
 }
 
-func (s *LinkLayerDeviceSerializationSuite) TestParsingSerializedData(c *gc.C) {
+func (s *LinkLayerDeviceSerializationSuite) TestParsingSerializedDataV1(c *gc.C) {
 	initial := linklayerdevices{
 		Version: 1,
 		LinkLayerDevices_: []*linklayerdevice{
@@ -65,6 +67,40 @@ func (s *LinkLayerDeviceSerializationSuite) TestParsingSerializedData(c *gc.C) {
 				IsAutoStart: true,
 				IsUp:        true,
 				ParentName:  "bam",
+			}),
+			newLinkLayerDevice(LinkLayerDeviceArgs{Name: "weeee"}),
+		},
+	}
+
+	bytes, err := yaml.Marshal(initial)
+	c.Assert(err, jc.ErrorIsNil)
+
+	var source map[string]interface{}
+	err = yaml.Unmarshal(bytes, &source)
+	c.Assert(err, jc.ErrorIsNil)
+
+	devices, err := importLinkLayerDevices(source)
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Assert(devices, jc.DeepEquals, initial.LinkLayerDevices_)
+}
+
+func (s *LinkLayerDeviceSerializationSuite) TestParsingSerializedDataV2(c *gc.C) {
+	initial := linklayerdevices{
+		Version: 2,
+		LinkLayerDevices_: []*linklayerdevice{
+			newLinkLayerDevice(LinkLayerDeviceArgs{
+				ProviderID:  "magic",
+				MachineID:   "bar",
+				Name:        "foo",
+				MTU:         54,
+				Type:        "loopback",
+				MACAddress:  "DEADBEEF",
+				IsAutoStart: true,
+				IsUp:        true,
+				ParentName:  "bam",
+				// V2 adds the VirtualPortType field
+				VirtualPortType: "ovs",
 			}),
 			newLinkLayerDevice(LinkLayerDeviceArgs{Name: "weeee"}),
 		},
